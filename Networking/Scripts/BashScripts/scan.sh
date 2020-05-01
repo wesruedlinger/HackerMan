021184d04cf11e3d3cb20377d8945d874a143099
Robert 'Wes' Ruedlinger <wes.ruedlinger91@gmail.com>
2020-05-01 16:08:25 -0400

    Delete scan.sh

--- Networking/Scripts/BashScripts/scan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..254}
-do
-	nc -nzvw1 $1.$i 0-65535 2>&1 &
-done | grep -E 'succ|open$'
-
-# sends all scans to background, then greps from the entire loop, 
-# looking for success or open at the end of the line
