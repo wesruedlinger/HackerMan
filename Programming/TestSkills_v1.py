cae6f0dd3fc0cecad43d5d85fe0c82c9a78650bd
Wes Ruedlinger <wes.ruedlinger91@gmail.com>
2021-02-05 12:23:32 -0500

    Update TestSkills_v1.py

--- Programming/TestSkills_v1.py
+++ Programming/TestSkills_v1.py
@@ -2,7 +2,6 @@
 
 def q1(floatstr):
     '''
-    TLO: 112-SCRPY002, LSA 3,4
     Given the floatstr, which is a comma separated string of
     floats, return a list with each of the floats in the
     argument as elements in the list.
@@ -14,8 +13,6 @@ def q1(floatstr):
 
 def q2(*args):
     '''
-    TLO: 112-SCRPY006, LSA 3
-    TLO: 112-SCRPY007, LSA 4
     Given the variable length argument list, return the average
     of all the arguments as a float
     '''
@@ -23,7 +20,6 @@ def q2(*args):
 
 def q3(lst,n):
     '''
-    TLO: 112-SCRPY004, LSA 3
     Given a list (lst) and a number of items (n), return a new
     list containing the last n entries in lst.
     '''
@@ -32,8 +28,6 @@ def q3(lst,n):
 
 def q4(strng):
     '''
-    TLO: 112-SCRPY004, LSA 1,2
-    TLO: 112-SCRPY006, LSA 3onward
     Given an input string, return a list containing the ordinal numbers of
     each character in the string in the order found in the input string.
     '''
@@ -44,8 +38,6 @@ def q4(strng):
 
 def q5(strng):
     '''
-    TLO: 112-SCRPY002, LSA 1,3
-    TLO: 112-SCRPY004, LSA 2
     Given an input string, return a tuple with each element in the tuple
     containing a single word from the input string in order.
     '''
@@ -54,8 +46,6 @@ def q5(strng):
 
 '''
 def q6():
-
-    TLO: 112-SCRPY006, LSA 4
     Given an input string similar to the below, craft a regular expression
     pattern to match and extract the date, time, and temperature in groups
     and return this pattern. Samples given below.
@@ -66,7 +56,6 @@ def q6():
 
 def q7(filename):
     '''
-    TLO: 112-SCRPY005, LSA 1
     Given a filename, open the file and return the length of the first line
     in the file excluding the line terminator.
     '''
@@ -77,9 +66,6 @@ def q7(filename):
 
 def q8(filename,lst):
     '''
-    TLO: 112-SCRPY003, LSA 1
-    TLO: 112-SCRPY004, LSA 1,2
-    TLO: 112-SCRPY005, LSA 1
     Given a filename and a list, write each entry from the list to the file
     on separate lines until a case-insensitive entry of "stop" is found in
     the list. If "stop" is not found in the list, write the entire list to
@@ -95,7 +81,6 @@ def q8(filename,lst):
 
 def q9(miltime):
     '''
-    TLO: 112-SCRPY003, LSA 1
     Given the military time in the argument miltime, return a string
     containing the greeting of the day.
     0300-1159 "Good Morning"
@@ -116,8 +101,6 @@ def q9(miltime):
 
 def q10(numlist):
     '''
-    TLO: 112-SCRPY003, LSA 1
-    TLO: 112-SCRPY004, LSA 1
     Given the argument numlist as a list of numbers, return True if all
     numbers in the list are NOT negative. If any numbers in the list are
     negative, return False.
